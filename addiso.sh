e0fdd504379f65961888b607ee096f6eb2aa6902
donaldbrian <65018791+donaldbrian@users.noreply.github.com>
2020-05-16 19:00:54 +0200

    Update addiso.sh

--- addiso.sh
+++ addiso.sh
@@ -8,9 +8,17 @@
 ########### To just test it, comment out the "echo "$grubentry" | sudo tee -a /etc/grub.d/40_custom" line ##########
 ########### and uncomment the following one  ###########
 ########### Do the same for the"sudo update-grub" one  ###########
-
 isoentry="$1";isoshort="${isoentry##*/}"
 echo A couple of checks:
+part=$(df -P . | sed -n '$s/[[:blank:]].*//p')
+par2=$(df -P / | sed -n '$s/[[:blank:]].*//p')
+
+if [ "$part" == "$par2" ]; then
+     isoinfo="$isoentry"
+else 
+     pw=$(echo $PWD | awk '{ print $1 }' | cut -c 6-)
+     isoinfo="$pw"/"$isoshort"
+fi
 
 FILE=$(which iso-info)
 if [ -f "$FILE" ]; then
@@ -18,8 +26,9 @@ if [ -f "$FILE" ]; then
 else 
     echo "iso-info is" $'\e[0;31m' "not installed." $'\e[0;37m' "Please [sudo apt] install libcdio-utils and run this again.";exit
 fi
+
 echo
-part=$(df -P . | sed -n '$s/[[:blank:]].*//p');p1=${part:7:1};p2=${part:8:1};p3=$(tr abcdefghij 0123456789 <<< "$p1");p4="hd"$p3,$p2
+p1=${part:7:1};p2=${part:8:1};p3=$(tr abcdefghij 0123456789 <<< "$p1");p4="hd"$p3,$p2
 ptype=${part:5:1}
 lz=$(iso-info -l -i $isoentry | grep -i initrd | awk '{ print $NF}')
 case $ptype in 
@@ -39,7 +48,7 @@ grubentry="$(cat <<-EOF
 #!/bin/sh
 exec tail -n +3 \$0
 menuentry "$isoshort" {
-        set isofile="$isoentry"
+        set isofile="$isoinfo"
         loopback loop ($p4)\$isofile
         linux (loop)/casper/vmlinuz boot=casper iso-scan/filename=\$isofile noprompt noeject toram
         initrd (loop)/casper/$lz
@@ -55,7 +64,7 @@ grubentry="$(cat <<-EOF
 #!/bin/sh
 exec tail -n +3 \$0
 menuentry "$isoshort" {
-        set isofile="$isoentry"
+        set isofile="$isoinfo"
         rmmod tpm
         loopback loop ($p4)\$isofile
         linux (loop)/casper/vmlinuz boot=casper iso-scan/filename=\$isofile noprompt noeject toram
@@ -100,4 +109,3 @@ echo $'\e[0;36m'
 echo "You should find an entry for "$isoshort" in your grub menu that should boot the OS"
 echo
 echo Close the window to exit.
-
